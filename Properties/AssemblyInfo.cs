0ab74f6c276f37f8b4e0f3e26bd493db3c65ed6f
Aleksandr <aleksandr.v.dikarev@gmail.com>
2022-08-02 12:39:48 +0300

    Create AssemblyInfo.cs

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("AzimuthSuite")]
+[assembly: AssemblyDescription("🐙 AzimuthSuite\r\n" +
+                               "Azimuth USBL (Zima 2) system host application\n\n" +
+                               "Latest release: https://github.com/ucnl/AzimuthSuite/releases \n" +
+                               "Support: mailto:support@unavlab.com\n\n" +
+                               "More information about Azimuth USBL system\n" +
+                               "English: https://docs.unavlab.com/navigation_and_tracking_systems_en.html#zima2\n" +
+                               "Russian: https://docs.unavlab.com/navigation_and_tracking_systems_ru.html#zima2\n\n" +
+                               "Project source code:\n" +
+                               "https://github.com/ucnl/AzimuthSuite\n\n" +
+                               "This application is 100% open-source, and based on our projects:\n" +
+                               "https://github.com/ucnl/UCNLNav\n" +
+                               "https://github.com/ucnl/UCNLPhysics\n" +
+                               "https://github.com/ucnl/UCNLNMEA\n" +
+                               "https://github.com/ucnl/UCNLKML\n" +
+                               "https://github.com/ucnl/UCNLDrivers\n" +
+                               "https://github.com/ucnl/UCNLUI\n" +
+                               "https://github.com/ucnl/UCNLSalinity")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Underwater communication & navigation laboratory, LLC")]
+[assembly: AssemblyProduct("AzimuthSuite")]
+[assembly: AssemblyCopyright("Copyright © Underwater communication & navigation laboratory, 2022")]
+[assembly: AssemblyTrademark("UC&NL")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("b0dcc246-58ce-470f-ac14-4b9a897407c9")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
